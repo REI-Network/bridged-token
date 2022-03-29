c7cace1294e5b1887c3cb49afbae919cc301ab26
samlior <samlior@foxmail.com>
2022-03-29 10:09:14 +0800

    chore: update deploy script

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -3,14 +3,22 @@ import type { DeployFunction } from 'hardhat-deploy/dist/types';
 const func: DeployFunction = async function ({ deployments, getNamedAccounts }) {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-  const token = await deploy('BridgedERC20', {
-    from: deployer,
-    log: true,
-    deterministicDeployment: false,
-    args: ['BridgedERC20', 'BridgedERC20', 18]
-  });
+  const deloyToken = async (name: string, symbol: string, decimals: number) => {
+    const token = await deploy('BridgedERC20', {
+      from: deployer,
+      log: true,
+      deterministicDeployment: false,
+      args: [name, symbol, decimals]
+    });
 
-  console.log('token at:', token.address);
+    console.log('name:', name, 'symbol:', symbol, 'decimals:', decimals, 'deployed at:', token.address);
+  };
+
+  await deloyToken('Wrapped BTC', 'WBTC', 8);
+  await deloyToken('Tether USD', 'USDT', 6);
+  await deloyToken('USD Coin', 'USDC', 6);
+  await deloyToken('Wrapped Ether', 'WETH', 18);
+  await deloyToken('Dai Stablecoin', 'DAI', 18);
 };
 
 export default func;
